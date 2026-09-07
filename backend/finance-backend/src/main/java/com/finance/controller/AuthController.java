8ed3a67ecbb6ba8e66ad99f44ba0a37624034b9f
Vutla Dhanush <vutladhanush21@gmail.com>
2026-09-07 15:24:28 +0530

    Update AuthController.java

--- backend/finance-backend/src/main/java/com/finance/controller/AuthController.java
+++ backend/finance-backend/src/main/java/com/finance/controller/AuthController.java
@@ -14,7 +14,7 @@ import java.util.Map;
 
 @RestController
 @RequestMapping("/api/auth")
-@CrossOrigin(origins = "http://localhost:5173")
+@CrossOrigin(origins = "https://finance-tracker-api-mauve.vercel.app/")
 public class AuthController {
 
     private final UserRepository userRepository;
