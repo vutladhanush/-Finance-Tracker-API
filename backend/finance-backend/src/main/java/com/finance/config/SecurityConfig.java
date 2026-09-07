f7f19daf6dcb12f18545b8afad2de4d441151631
Vutla Dhanush <vutladhanush21@gmail.com>
2026-09-07 15:33:03 +0530

    Update SecurityConfig.java

--- backend/finance-backend/src/main/java/com/finance/config/SecurityConfig.java
+++ backend/finance-backend/src/main/java/com/finance/config/SecurityConfig.java
@@ -57,7 +57,7 @@ public class SecurityConfig {
                 new CorsConfiguration();
 
         configuration.setAllowedOrigins(
-                List.of( "https://finance-tracker-api-mauve.vercel.app/)
+                List.of( "https://finance-tracker-api-mauve.vercel.app/")
         );
 
         configuration.setAllowedMethods(
